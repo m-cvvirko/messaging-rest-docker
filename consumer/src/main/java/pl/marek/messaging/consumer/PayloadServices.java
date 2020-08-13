a6e817bd6751344c5d3e08d144c21ad07c016742
m-cvvirko <marek@cvvirko.pl>
2020-08-13 08:40:25 +0200

    return to state then consumer made only echo of request

--- consumer/src/main/java/pl/marek/messaging/consumer/PayloadServices.java
+++ consumer/src/main/java/pl/marek/messaging/consumer/PayloadServices.java
@@ -20,16 +20,16 @@ public class PayloadServices {
     }
 
     public Map<String, Object> process(Map<String, Object> jsonClients) {
-        Map<String, Object> jsonObjClients = (Map<String, Object>)jsonClients.get("clients");
-        Map<String, Object> jsonObjClient = (Map<String, Object>)jsonObjClients.get("client");
-
-        Map<String, Object> clientInfo = (Map<String, Object>)jsonObjClient.get("info");
-        Map<String, Object> clientBalance = (Map<String, Object>)jsonObjClient.get("balance");
-        Map<String, Object> clientTransactions = (Map<String, Object>)jsonObjClient.get("transactions");
-        Client client = new Client(clientInfo, clientBalance, clientTransactions);
-        repository.save(client);
-        List<Client> response = computeResponse(clientInfo);
-        return clientBalance;
+//        Map<String, Object> jsonObjClients = (Map<String, Object>)jsonClients.get("clients");
+//        Map<String, Object> jsonObjClient = (Map<String, Object>)jsonObjClients.get("client");
+//
+//        Map<String, Object> clientInfo = (Map<String, Object>)jsonObjClient.get("info");
+//        Map<String, Object> clientBalance = (Map<String, Object>)jsonObjClient.get("balance");
+//        Map<String, Object> clientTransactions = (Map<String, Object>)jsonObjClient.get("transactions");
+//        Client client = new Client(clientInfo, clientBalance, clientTransactions);
+//        repository.save(client);
+//        List<Client> response = computeResponse(clientInfo);
+        return jsonClients;
     }
 
     private List<Client> computeResponse(Map<String, Object> client) {
